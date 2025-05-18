b889dc486cd51b5a6ce0490f444a379704bc93af
fazlehasan <fsdigitalgraphic@gmail.com>
2025-05-18 21:04:22 +0600

    OOP Taliwn BTN make

---
+++ twbg/btng.js
@@ -0,0 +1,9 @@
+class TailwindButtonGenerator{
+    static createButton(color, text,size){
+        const button =`
+        <button class="bg-${color}-500 hover:bg-${color}-700 text-white font-bold py-${size} px-${size} mx-${size} rounded ">${text}</button>
+        `
+
+        return button
+    }
+}
